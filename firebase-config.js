2cba7e74c16b2304161263b78b10045f8536b496
dow42 <sonowalkshitis@gmail.com>
2025-05-28 09:48:41 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDOe67QxfmFnzj7hVOFrYdaLKrvNMUawWU",
+  authDomain: "asomiyabondhu.firebaseapp.com",
+  projectId: "asomiyabondhu",
+  storageBucket: "asomiyabondhu.appspot.com",
+  messagingSenderId: "413467477694",
+  appId: "1:413467477694:web:d1d4d16d7a574eeb8900ef",
+  measurementId: "G-WS7FK2EL4K"
+};
+firebase.initializeApp(firebaseConfig);
