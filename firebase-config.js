6326d14b7096eb02628e7961a1d212d713e546b4
dow42 <sonowalkshitis@gmail.com>
2025-05-28 12:14:42 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDOe67QxfmFnzj7hVOFrYdaLKrvNMUawWU",
-  authDomain: "asomiyabondhu.firebaseapp.com",
-  projectId: "asomiyabondhu",
-  storageBucket: "asomiyabondhu.firebasestorage.app",
-  messagingSenderId: "413467477694",
-  appId: "1:413467477694:web:95a4689a196890188900ef",
-  measurementId: "G-MEJGJR9Y05"
-};
