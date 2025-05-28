4aa4e3a3f761d70f7d140bbeef2f45881879ea6a
dow42 <sonowalkshitis@gmail.com>
2025-05-28 10:16:35 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyDOe67QxfmFnzj7hVOFrYdaLKrvNMUawWU",
   authDomain: "asomiyabondhu.firebaseapp.com",
   projectId: "asomiyabondhu",
-  storageBucket: "asomiyabondhu.appspot.com",
+  storageBucket: "asomiyabondhu.firebasestorage.app",
   messagingSenderId: "413467477694",
-  appId: "1:413467477694:web:d1d4d16d7a574eeb8900ef",
-  measurementId: "G-WS7FK2EL4K"
+  appId: "1:413467477694:web:95a4689a196890188900ef",
+  measurementId: "G-MEJGJR9Y05"
 };
-firebase.initializeApp(firebaseConfig);
-firebase.firestore();
